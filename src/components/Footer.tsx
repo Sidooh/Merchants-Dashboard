c32446b6a1a657d5eaa27d53696405bf2c5d56b4
Nabcellent <nabcellent.dev@gmail.com>
2023-10-19 09:54:45 +0300

    Initial Commit
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,20 @@
 import { CONFIG } from '@/config.ts';
+import moment from 'moment';
 
 const Footer = () => {
     return (
         <footer className="absolute bottom-0 w-full">
             <div className="container flex justify-between text-xs mt-4 mb-3">
                 <p className="mb-0 text-600">
-                    {CONFIG.app.name} | <a href="/">Sidooh</a> <br className="sm:hidden" /> &copy;{' '}
-                    {new Date().getFullYear()}
+                    {CONFIG.app.name} |{' '}
+                    <a href="/" className={'text-primary'}>
+                        Sidooh
+                    </a>{' '}
+                    <br className="sm:hidden" /> &copy; {new Date().getFullYear()}
                 </p>
-                <p className="mb-0 text-600">v{CONFIG.app.version}</p>
+                <p className="mb-0 text-600">
+                    v{CONFIG.app.version} | {moment().format('dddd, Do MMMM')}
+                </p>
             </div>
         </footer>
     );
